fe82e3025cc2aac5401c66004efc1cd471a256c2
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-04-06 20:15:08 -0300

    add

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    var question :[Question]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let q0a1 = Answer(answer:"120 anos", isCorrect:true)
+        let q0a2 = Answer(answer:"80 anos", isCorrect:false)
+        let q0a3 = Answer(answer:"140 anos", isCorrect:false)
+        let q0a4 = Answer(answer:"100 anos", isCorrect:false)
+        
+        let q0 = Question(question:"Quantos anos vive um elefante africano?", strImageFileName:"Elefante", answers:[q0a1,q0a2,q0a3,q0a4])
+        question = [q0]
+        //ShowQuestion(0)
+        
     }
+    
 
+    
 
 }
 
