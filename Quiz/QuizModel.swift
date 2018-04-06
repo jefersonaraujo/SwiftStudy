9575d8ff7ddae88ea516fa09b8ec65e243f3ec7f
Ios <ios@FA7-L40M16.local>
2018-04-06 20:00:17 -0300

    add

---
+++ Quiz/QuizModel.swift
@@ -0,0 +1,37 @@
+//
+//  QuizModel.swift
+//  Quiz
+//
+//  Created by Ios on 06/04/2018.
+//  Copyright © 2018 INeedSolutions. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Question {
+    
+    var strQuestion :String!
+    var imgQuestion :UIImage!
+    var answers : [Answer]!
+    
+    init(question:String, strImageFileName:String, answers:[Answer]){
+        
+        self.strQuestion = question
+        self.imgQuestion = UIImage(named:strImageFileName)
+        self.answers = answers
+    }
+    
+}
+
+class Answer {
+    
+    var strAnswer:String!
+    var isCorrect:Bool!
+    
+    init(answer:String, isCorrect: Bool) {
+        
+        self.strAnswer = answer
+        self.isCorrect = isCorrect
+    }
+}
