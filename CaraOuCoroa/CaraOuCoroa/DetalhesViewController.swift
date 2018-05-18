732892d4c5eacea5f64ee171ea161588a2aa5b37
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-05-18 20:21:34 -0300

    chamada de tela

---
+++ CaraOuCoroa/CaraOuCoroa/DetalhesViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetalhesViewController.swift
+//  CaraOuCoroa
+//
+//  Created by ios on 18/05/18.
+//  Copyright © 2018 Ineesolutions.com.br. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Classe Detalhe View Controller
+class DetalhesViewController: UIViewController {
+    
+    
+    @IBOutlet var imgMoedas: UIView!
+    var numeroRecebido:Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        if  numeroRecebido == 0{
+            
+            
+          
+        }else{
+            
+        }
+    }
+    
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
